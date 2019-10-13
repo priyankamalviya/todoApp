64dd5af0799acc0007e09d3f4fd38558a3d960a5
Priyanka Malviya <priyanka_malviya@icloud.com>
2019-10-13 09:22:23 -0700

    add focus, changes styles

--- index.js
+++ index.js
@@ -5,26 +5,37 @@ const listContainer = document.querySelector('#list-container');
 let todoEl = '';
 let listItem = '';
 
+window.onload = function () {
+    inputText.focus();
+};
 const handleAddTodo = (e) => {
     todoEl = inputText.value;
     listItem = `<li class="todo-item">
+        <p>
+        ${todoEl}
+        </p>
         <i
         class="fa fa-check-circle-o"
         aria-hidden="true"
         id="unchecked-box"
         ></i>
-        <p>
-        ${todoEl}
-        </p>
         <i class="fa fa-trash" aria-hidden="true" id="remove-todo"></i>
         </li>`;
     const frag = document.createRange().createContextualFragment(listItem);
     listContainer.appendChild(frag);
     inputText.value = "";
+    inputText.focus();
 }
 
 addTodo.addEventListener("click", handleAddTodo);
 
+// handle adding to do on enter
+inputText.addEventListener("keypress", (e) => {
+    e = e || window.event;
+    if (e.keyCode === 13) {
+        handleAddTodo();
+    }
+});
 
 listContainer.addEventListener("click", (e) => {
     var tgt = e.target;
@@ -34,8 +45,7 @@ listContainer.addEventListener("click", (e) => {
 
     if (tgt.classList.contains('fa-check-circle-o')) {
         tgt.classList.replace('fa-check-circle-o', 'fa-check-circle');
-        console.log(tgt.nextElementSibling.style)
-        tgt.nextElementSibling.style.textDecoration = "line-through";
-        tgt.nextElementSibling.style.color = 'gray';
+        tgt.previousElementSibling.style.textDecoration = "line-through";
+        tgt.previousElementSibling.style.color = 'gray';
     }
 });
